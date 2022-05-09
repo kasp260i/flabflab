0165f8971c00b6dd2129d932a81311e49e9ec524
kasp260i <51888040+kasp260i@users.noreply.github.com>
2022-05-09 09:18:53 +0200

    Add files via upload

---
+++ firework.js
@@ -0,0 +1,80 @@
+class Firework extends GameObject {
+ 
+    static spawnInterval = 500;
+    static timeSinceLastSpawn = Firework.spawnInterval;
+ 
+    constructor() {
+        let drawOrder = 10;
+        let tag = "firework";
+        super(drawOrder, tag);
+        this.sound = new Audio("../assets/sounds/coin.wav");
+        this.image = new Image(60, 60);
+        this.image.src = "../assets/images/firework.png";
+        this.hitboxRadius = 30;
+        this.xSpeed = -10;
+        this.value = 5;
+        this.xPosition = spawnXPosition;
+        this.yPosition = randomBetween(0, canvas.height);
+        this.yOffset = 0;
+        this.oscillilationScale = randomBetween(0, 200);
+        this.oscillilationTime = 0;
+        this.oscillilationSpeed = randomBetween(0, 0.05);
+    }
+ 
+    draw() {
+        drawImage(
+            this.image,
+            this.xPosition,
+            this.yPosition + this.yOffset,
+            this.image.width,
+            this.image.height
+        );
+ 
+        if(debugModeIsOn) {
+            drawCircle(
+                this.xPosition,
+                this.yPosition + this.yOffset,
+                this.hitboxRadius,
+                hitboxOND
+            );
+        }
+    }
+ 
+    update() {
+        this.xPosition += this.xSpeed;
+        this.oscillate();
+
+
+        if(this.xPosition < destructionXPosition) {
+            this.destroy();
+        }
+
+        if  (
+            theseCirclesCollide(
+                bird.xPosition,
+                bird.yPosition,
+                bird.hitboxRadius,
+                this.xPosition,
+                this.yPosition + this.yOffset,
+                this.hitboxRadius)
+            && gameState == "action") 
+        {
+       liv = liv-1;
+       this.destroy()
+       if(liv==0){
+        bird.canFlap = false;
+        gameOverSound.play();
+        flapText.isActive = false;
+        gameOverText.isActive = true;
+        gameState = "gameover";
+        
+        }
+        }
+        
+    }
+ 
+    oscillate() {
+        this.oscillilationTime += this.oscillilationSpeed;
+        this.yOffset = Math.sin(this.oscillilationTime) * this.oscillilationScale;
+    }
+}
